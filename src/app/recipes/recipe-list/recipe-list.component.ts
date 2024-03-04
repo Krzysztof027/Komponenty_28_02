fc7807fc5ab0472d97312c67062c0a6e3bfee444
Krzysztof27 <krzysztofszczerkowski1492@gmail.com>
2024-03-04 09:53:51 +0100

    Komponenty - wiazania

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
+import { Recipe  } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [new Recipe('A Test Recipe' , 'This is simply a test',
+   'https://th.bing.com/th/id/OIP.jtsOYO2I5uaO9OXWMIy5CQHaE7?rs=1&pid=ImgDetMain'), new Recipe('A Test Recipe' , 'This is simply a test',
+   'https://th.bing.com/th/id/OIP.jtsOYO2I5uaO9OXWMIy5CQHaE7?rs=1&pid=ImgDetMain')];
+    
+  
 }
