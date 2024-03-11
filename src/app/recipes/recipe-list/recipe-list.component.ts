820ff217e5489f822f2923f4584aba38b2364018
Krzysztof27 <krzysztofszczerkowski1492@gmail.com>
2024-03-11 09:54:32 +0100

    Event nie dziala

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 import { Recipe  } from '../recipe.model';
 
@@ -8,9 +8,20 @@ import { Recipe  } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
-  recipes: Recipe[] = [new Recipe('A Test Recipe' , 'This is simply a test',
-   'https://th.bing.com/th/id/OIP.jtsOYO2I5uaO9OXWMIy5CQHaE7?rs=1&pid=ImgDetMain'), new Recipe('A Test Recipe' , 'This is simply a test',
-   'https://th.bing.com/th/id/OIP.jtsOYO2I5uaO9OXWMIy5CQHaE7?rs=1&pid=ImgDetMain')];
+
+  recipeWaSelected = new EventEmitter<Recipe>(); 
+
+  @Output() recipes: Recipe[] = [new Recipe('A Test Recipe' , 'This is simply a test',
+   'https://th.bing.com/th/id/OIP.jtsOYO2I5uaO9OXWMIy5CQHaE7?rs=1&pid=ImgDetMain'),
+    new Recipe('A Test Recipe' , 'This is simply a test',
+   'https://th.bing.com/th/id/OIP.jtsOYO2I5uaO9OXWMIy5CQHaE7?rs=1&pid=ImgDetMain')
+  
+  ];
     
+  
+   OnRecipeSelected(recipe: Recipe){
+    this.recipeWaSelected.emit(recipe);
+
+   }
   
 }
